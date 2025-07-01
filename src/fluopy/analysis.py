11d58b1115489dce30f4050575e07ff03892dc30
Sagix7 <85253529+Sagix7@users.noreply.github.com>
2025-07-01 08:23:55 +0200

    test black on push

--- src/fluopy/analysis.py
+++ src/fluopy/analysis.py
@@ -12,7 +12,8 @@ import pandas as pd
 from . import figure as fi
 from .miscellaneous import format_electronic_state, format_transition
 
-
+def test_black_on_push(x
+                    ,y): return True 
 class Analysis:
     """
     Container of simulation-dervied statistical attributes and methods.
