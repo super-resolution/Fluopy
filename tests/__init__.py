0c6b9b8cc9abd7171a74540afe3c75893d91bb6b
sdoose <soeren.doose@uni-wuerzburg.de>
2025-07-08 12:51:53 +0200

    refactor: test module description

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,6 @@
-# comment to increase file size > 0 B
+"""
+Test the fluopy package.
+
+This module includes unit tests for all modules within the fluopy package.
+The tests are organized following the subpackage structure of fluopy.
+"""
